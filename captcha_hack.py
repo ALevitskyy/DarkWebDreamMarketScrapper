425d10652070f33d340bd75904afd1abb348e3f4
Andriy Levitskyy <levitskyyandriy@gmail.com>
2020-03-11 22:12:42 -0400

    code formatting using black

--- captcha_hack.py
+++ captcha_hack.py
@@ -17,32 +17,44 @@ import pytesseract
 import re
 import time
 from collections import Counter
-def too_many_windows(browser,main_window):
+
+
+def too_many_windows(browser, main_window):
     windows = browser.window_handles
-    while len(windows)>2:
+    while len(windows) > 2:
         browser.switch_to.window(windows[2])
         browser.close()
         windows = browser.window_handles
     browser.switch_to_window(main_window)
+
+
 def get_browser(binary=None):
-    
-    global browser  
+
+    global browser
     # only one instance of a browser opens, remove global for multiple instances
-    if not browser: 
+    if not browser:
         browser = webdriver.Firefox(firefox_binary=binary)
     return browser
+
+
 def trim(im):
-   bg = Image.new(im.mode, im.size, im.getpixel((0,0)))
-   diff = ImageChops.difference(im, bg)
-   diff = ImageChops.add(diff, diff, 2.0, -100)
-   bbox = diff.getbbox()
-   if bbox:
-       return im.crop(bbox)
+    bg = Image.new(im.mode, im.size, im.getpixel((0, 0)))
+    diff = ImageChops.difference(im, bg)
+    diff = ImageChops.add(diff, diff, 2.0, -100)
+    bbox = diff.getbbox()
+    if bbox:
+        return im.crop(bbox)
+
+
 def change_contrast(img, level):
     factor = (259 * (level + 255)) / (255 * (259 - level))
+
     def contrast(c):
         return 128 + factor * (c - 128)
+
     return img.point(contrast)
+
+
 def clean_captcha(filename):
     captcha = filename
     initial_im = Image.open(captcha).convert("P")
@@ -54,97 +66,112 @@ def clean_captcha(filename):
             if pix > 220:
                 cleaned_im.putpixel((y, x), 0)
     cleaned_im.save("cleaned.png")
+
+
 def get_text():
-    param_space=np.linspace(1,3.2,40)
-#    param_space2=np.linspace(350,450,5)
-    z=400
-    poss_results=[]
+    param_space = np.linspace(1, 3.2, 40)
+    #    param_space2=np.linspace(350,450,5)
+    z = 400
+    poss_results = []
     for i in param_space:
-        final_im=change_contrast(
-                Image.fromarray(ndimage.filters.gaussian_filter(
-                Image.open("cleaned.png"), i, mode='nearest')),z)
+        final_im = change_contrast(
+            Image.fromarray(
+                ndimage.filters.gaussian_filter(
+                    Image.open("cleaned.png"), i, mode="nearest"
+                )
+            ),
+            z,
+        )
         text = pytesseract.image_to_string(final_im)
-        text=re.sub(r'[^\w]', '', str(text))
-        if len(str(text))==4:
+        text = re.sub(r"[^\w]", "", str(text))
+        if len(str(text)) == 4:
             poss_results.append(str(text))
     try:
-#        first=[x[0] for x in poss_results]
-#        second=[x[1] for x in poss_results]
- #       third=[x[2] for x in poss_results]
- #       fourth=[x[3] for x in poss_results]
- #       return(Counter(first).most_common(1)[0][0]+Counter(second).most_common(1)[0][0]+
- #     Counter(third).most_common(1)[0][0]+Counter(fourth).most_common(1)[0][0])
-        return(Counter(poss_results).most_common(1)[0][0])
+        #        first=[x[0] for x in poss_results]
+        #        second=[x[1] for x in poss_results]
+        #       third=[x[2] for x in poss_results]
+        #       fourth=[x[3] for x in poss_results]
+        #       return(Counter(first).most_common(1)[0][0]+Counter(second).most_common(1)[0][0]+
+        #     Counter(third).most_common(1)[0][0]+Counter(fourth).most_common(1)[0][0])
+        return Counter(poss_results).most_common(1)[0][0]
     except:
         return "fail"
-def get_captcha_text(captcha_im,browser):
+
+
+def get_captcha_text(captcha_im, browser):
     try:
         location = captcha_im.location
         size = captcha_im.size
         png = browser.get_screenshot_as_png()
         im = Image.open(BytesIO(png))
-        im.save('screenshot2.png')
-        im = Image.open('screenshot2.png')
-#need to handcode here
-        location['x']=548-20
-#location['y']=690-542
-        location['y']=450
-        left = location['x']
-        top = location['y']
-        right = location['x'] + size['width']+140+40
-        bottom = location['y'] + size['height']+100
-        im = im.crop((left, top, right, bottom)) # defines crop points
+        im.save("screenshot2.png")
+        im = Image.open("screenshot2.png")
+        # need to handcode here
+        location["x"] = 548 - 20
+        # location['y']=690-542
+        location["y"] = 450
+        left = location["x"]
+        top = location["y"]
+        right = location["x"] + size["width"] + 140 + 40
+        bottom = location["y"] + size["height"] + 100
+        im = im.crop((left, top, right, bottom))  # defines crop points
         im.save("screenshot.png")
-        opencvImage = cv2.cvtColor(np.array(im), cv2.COLOR_RGB2BGR) 
-        img=opencvImage    
+        opencvImage = cv2.cvtColor(np.array(im), cv2.COLOR_RGB2BGR)
+        img = opencvImage
         gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-        ret,thresh = cv2.threshold(gray,127,255,1) 
-        image, contours, hierarchy = cv2.findContours(thresh,1,2)
- 
+        ret, thresh = cv2.threshold(gray, 127, 255, 1)
+        image, contours, hierarchy = cv2.findContours(thresh, 1, 2)
+
         for cnt in contours:
-            if cv2.contourArea(cnt)>10000:
-                cv2.drawContours(img,[cnt],0,255,-1)
-                (x,y,w,h) = cv2.boundingRect(cnt)
-                crop_img = gray[y:y+h, x:x+w]
+            if cv2.contourArea(cnt) > 10000:
+                cv2.drawContours(img, [cnt], 0, 255, -1)
+                (x, y, w, h) = cv2.boundingRect(cnt)
+                crop_img = gray[y : y + h, x : x + w]
                 cv2.imwrite("cropped.png", crop_img)
                 break
-        cv2.imwrite("countours.png",img)
-        ima = Image.open("cropped.png") # the second one 
+        cv2.imwrite("countours.png", img)
+        ima = Image.open("cropped.png")  # the second one
         ima = ima.filter(ImageFilter.MedianFilter())
         enhancer = ImageEnhance.Contrast(ima)
         ima = enhancer.enhance(2)
-        ima = ima.convert('1')
-        ima.save('temp2.jpg')
-        tirmmed=trim(ima)
+        ima = ima.convert("1")
+        ima.save("temp2.jpg")
+        tirmmed = trim(ima)
         tirmmed.save("trimmed.jpg")
         clean_captcha("trimmed.jpg")
-        text=get_text()
-        return(text)
+        text = get_text()
+        return text
     except:
         return "fail"
-#os.environ["PATH"]="/Users/andriylevitskyy/Desktop/helping_heather/"
+
+
+# os.environ["PATH"]="/Users/andriylevitskyy/Desktop/helping_heather/"
 
 # path to the firefox binary inside the Tor package
-#binary = '/Applications/TorBrowser.app/Contents/MacOS/firefox'
-#binary = '/Applications/TorBrowser.app/Contents/MacOS/firefox'
-#Copied from stackoverfolow- initiating Tor Browser
-#if os.path.exists(binary) is False:
+# binary = '/Applications/TorBrowser.app/Contents/MacOS/firefox'
+# binary = '/Applications/TorBrowser.app/Contents/MacOS/firefox'
+# Copied from stackoverfolow- initiating Tor Browser
+# if os.path.exists(binary) is False:
 #    raise ValueError("The binary path to Tor firefox does not exist.")
-#firefox_binary = FirefoxBinary(binary)
-#browser = None
-#browser = get_browser(binary=firefox_binary)
-#url='https://check.torproject.org/'
-#url='file:///Users/andriylevitskyy/Desktop/helping_heather/Verify%20Human.html'
-#browser.get(url)
+# firefox_binary = FirefoxBinary(binary)
+# browser = None
+# browser = get_browser(binary=firefox_binary)
+# url='https://check.torproject.org/'
+# url='file:///Users/andriylevitskyy/Desktop/helping_heather/Verify%20Human.html'
+# browser.get(url)
 def dos_actions(browser):
-    math=browser.find_element_by_css_selector("div.iline:nth-child(3)")
-    result=browser.find_element_by_css_selector("#result")
-    captcha=browser.find_element_by_css_selector("#captcha")
-    captcha_im=browser.find_element_by_css_selector("div.inl:nth-child(1) > label:nth-child(1) > img:nth-child(1)")
+    math = browser.find_element_by_css_selector("div.iline:nth-child(3)")
+    result = browser.find_element_by_css_selector("#result")
+    captcha = browser.find_element_by_css_selector("#captcha")
+    captcha_im = browser.find_element_by_css_selector(
+        "div.inl:nth-child(1) > label:nth-child(1) > img:nth-child(1)"
+    )
     result.send_keys(eval(math.text.strip("Challenge")))
-    captcha.send_keys(get_captcha_text(captcha_im,browser))
+    captcha.send_keys(get_captcha_text(captcha_im, browser))
     time.sleep(3)
     browser.find_element_by_css_selector(".sdiv > input:nth-child(1)").click()
+
+
 def if_dos(browser):
     try:
         browser.find_element_by_css_selector(".ddos")
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-#All the relevant libraries are attached below here
+# All the relevant libraries are attached below here
 import os
 from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
 from selenium import webdriver
@@ -7,156 +7,171 @@ from selenium.webdriver.common.action_chains import ActionChains
 import time
 import datetime
 import csv
-from captcha_hack import if_dos,dos_actions,too_many_windows
-#Seeting the geckodriver path
-os.environ["PATH"]="/Users/heather/Documents/Scraping/Gecko driver/"
+from captcha_hack import if_dos, dos_actions, too_many_windows
+
+# Seeting the geckodriver path
+os.environ["PATH"] = "/Users/heather/Documents/Scraping/Gecko driver/"
 
 # path to the firefox binary inside the Tor package
-#binary = '/Applications/TorBrowser.app/Contents/MacOS/firefox'
-binary = '/Applications/TorBrowser.app/Contents/MacOS/firefox'
-#Copied from stackoverfolow- initiating Tor Browser
+# binary = '/Applications/TorBrowser.app/Contents/MacOS/firefox'
+binary = "/Applications/TorBrowser.app/Contents/MacOS/firefox"
+# Copied from stackoverfolow- initiating Tor Browser
 if os.path.exists(binary) is False:
     raise ValueError("The binary path to Tor firefox does not exist.")
 firefox_binary = FirefoxBinary(binary)
 browser = None
 
-#Copied from stackoverflow, initiates the tor browser
+# Copied from stackoverflow, initiates the tor browser
 def get_browser(binary=None):
-    
-    global browser  
+
+    global browser
     # only one instance of a browser opens, remove global for multiple instances
-    if not browser: 
+    if not browser:
         browser = webdriver.Firefox(firefox_binary=binary)
     return browser
-#Support functions which are used to get the information from the page
+
+
+# Support functions which are used to get the information from the page
 def get_title(browser):
-    #Try except makes sure scapper does not die when it encounters a bug 
+    # Try except makes sure scapper does not die when it encounters a bug
     try:
-        #Finds the element with class title and get the textual information from it
-       title=browser.find_element_by_class_name('title') 
-       return title.text
+        # Finds the element with class title and get the textual information from it
+        title = browser.find_element_by_class_name("title")
+        return title.text
     except:
         return "NA"
+
+
 def get_basic_info(browser):
-    inidividual_item=[]
-    #find element with clas tabularDetails. 
-    #THen looks for all children divs and gets textual information
-    tabularDetails=browser.find_element_by_class_name('tabularDetails')
+    inidividual_item = []
+    # find element with clas tabularDetails.
+    # THen looks for all children divs and gets textual information
+    tabularDetails = browser.find_element_by_class_name("tabularDetails")
     # Be careful DISTINGUISHING find_ELEMENTS and find_ELEMENT
     for element in tabularDetails.find_elements_by_tag_name("div"):
         individual_item.append(element.text)
     return inidividual_item
+
+
 def get_product_description(browser):
     try:
-        #Find element with id offerDescription and gets the text
-        product_description=browser.find_element_by_id('offerDescription')
+        # Find element with id offerDescription and gets the text
+        product_description = browser.find_element_by_id("offerDescription")
         return product_description.text
     except:
         return "NA"
+
+
 def get_terms_and_conditions(browser):
     try:
-        #Counld not find a good 
-        terms_and_conditions=browser.find_element_by_css_selector(".viewProduct > div:nth-child(8) > div:nth-child(2) > pre:nth-child(1)")
-        return(terms_and_conditions.text)
+        # Counld not find a good
+        terms_and_conditions = browser.find_element_by_css_selector(
+            ".viewProduct > div:nth-child(8) > div:nth-child(2) > pre:nth-child(1)"
+        )
+        return terms_and_conditions.text
     except:
         return "NA"
-def get_shipping_options_and_currencies(browser,individual_item):
+
+
+def get_shipping_options_and_currencies(browser, individual_item):
     try:
-        new_thing=individual_item
-        shipping_options=browser.find_elements_by_class_name('shippingTable')
-        counter=0
+        new_thing = individual_item
+        shipping_options = browser.find_elements_by_class_name("shippingTable")
+        counter = 0
         for z in shipping_options:
-            result2=[]
+            result2 = []
             for i in z.find_elements_by_tag_name("tr"):
                 result2.append(i.text)
             new_thing.append(result2)
-            counter+=1
-        if counter==1:
+            counter += 1
+        if counter == 1:
             new_thing.append("NA")
-        
+
         return new_thing
     except:
-        return individual_item+["NA","NA"]
-    
+        return individual_item + ["NA", "NA"]
+
+
 def get_product_ratings(browser):
-    result=[]
+    result = []
     try:
-        ratings=browser.find_element_by_class_name('ratings')
+        ratings = browser.find_element_by_class_name("ratings")
     except:
         return "NA"
     try:
         for tr in ratings.find_elements_by_tag_name("tr"):
-            interm=[]
-            interm.append(tr.find_element_by_class_name('age').text)
-            stars=tr.find_element_by_class_name('rating')
-            counter=0
-            for star in stars.find_elements_by_class_name('star'):
-                counter+=1
-            interm.append(str(counter)+" stars")
-            interm.append(tr.find_element_by_class_name('ratingText').text)
-            result.append(interm) 
+            interm = []
+            interm.append(tr.find_element_by_class_name("age").text)
+            stars = tr.find_element_by_class_name("rating")
+            counter = 0
+            for star in stars.find_elements_by_class_name("star"):
+                counter += 1
+            interm.append(str(counter) + " stars")
+            interm.append(tr.find_element_by_class_name("ratingText").text)
+            result.append(interm)
         return result
     except:
         return "NA"
 
+
 browser = get_browser(binary=firefox_binary)
-#url='https://check.torproject.org/'
-url='http://fkqzda67aavjkhui.onion/?ai=1675'
+# url='https://check.torproject.org/'
+url = "http://fkqzda67aavjkhui.onion/?ai=1675"
 browser.get(url)
-#Example with prices
-#time.sleep(60)
-#for timer in range(15):
+# Example with prices
+# time.sleep(60)
+# for timer in range(15):
 #    prices=browser.find_elements_by_class_name('oPrice')
 #    for i in prices:
 #        print(i.text)
 #    time.sleep(5)
-#    elem=browser.find_element_by_class_name('lastPager')   
+#    elem=browser.find_element_by_class_name('lastPager')
 #    elem.click()
 #    time.sleep(5)
-titles=[]
-counter=0
-counter2=0
+titles = []
+counter = 0
+counter2 = 0
 time.sleep(60)
 while True:
-    #finds al the images on the main_page
+    # finds al the images on the main_page
     while True:
-        try:    
-            images=browser.find_elements_by_class_name('oImage')
+        try:
+            images = browser.find_elements_by_class_name("oImage")
             break
         except:
             browser.refresh()
             time.sleep(5)
             continue
-    #need to return to main_page after the page is closing
+    # need to return to main_page after the page is closing
     main_window = browser.current_window_handle
     for image in images:
-        
-        #Emulates keybord, presses shift to open a new window
+
+        # Emulates keybord, presses shift to open a new window
         while True:
             try:
-                counter+=1
+                counter += 1
                 print(counter)
                 image.click()
                 time.sleep(2)
-        #Referes to "VIEW OFFER button"
-                view_offer=browser.find_element_by_id('openProduct')
-                ActionChains(browser) \
-                .key_down(Keys.SHIFT) \
-                .click(view_offer) \
-                .key_up(Keys.SHIFT) \
-                .perform()
+                # Referes to "VIEW OFFER button"
+                view_offer = browser.find_element_by_id("openProduct")
+                ActionChains(browser).key_down(Keys.SHIFT).click(view_offer).key_up(
+                    Keys.SHIFT
+                ).perform()
                 time.sleep(3)
-        #Shift to the new window with specific product info
+                # Shift to the new window with specific product info
                 windows = browser.window_handles
                 browser.switch_to.window(windows[1])
                 time.sleep(0.5)
-            #Get all the information into list called INDIVIDUAL_ITEM
-                individual_item=[]
+                # Get all the information into list called INDIVIDUAL_ITEM
+                individual_item = []
                 individual_item.append(get_title(browser))
-                individual_item=individual_item+get_basic_info(browser)
+                individual_item = individual_item + get_basic_info(browser)
                 individual_item.append(get_product_description(browser))
                 individual_item.append(get_terms_and_conditions(browser))
-                individual_item=get_shipping_options_and_currencies(browser,individual_item)
+                individual_item = get_shipping_options_and_currencies(
+                    browser, individual_item
+                )
                 individual_item.append(get_product_ratings(browser))
                 individual_item.append(str(datetime.datetime.now()))
                 break
@@ -171,36 +186,35 @@ while True:
                     browser.switch_to_window(main_window)
                     time.sleep(0.5)
                     continue
-        #APPEND INDIVIDUAL_ITEM to the csv file
-        with open("output.csv", "a",encoding="utf-8") as fp:
-            wr = csv.writer(fp, dialect='excel')
+        # APPEND INDIVIDUAL_ITEM to the csv file
+        with open("output.csv", "a", encoding="utf-8") as fp:
+            wr = csv.writer(fp, dialect="excel")
             wr.writerow(individual_item)
-        #Closing the specific information windown and switching back to the main window
+        # Closing the specific information windown and switching back to the main window
         browser.close()
         time.sleep(0.5)
         browser.switch_to_window(main_window)
     time.sleep(3)
-    current_url=browser.current_url
-    too_many_windows(browser,main_window)
+    current_url = browser.current_url
+    too_many_windows(browser, main_window)
     try:
-        #when there is end of the page switch to the new page
-        elem=browser.find_element_by_class_name('lastPager')  
+        # when there is end of the page switch to the new page
+        elem = browser.find_element_by_class_name("lastPager")
         elem.click()
         time.sleep(5)
-        counter2+=1
+        counter2 += 1
         print(counter2)
     except:
         time.sleep(5)
         if if_dos(browser):
             dos_actions(browser)
             browser.get(current_url)
-            elem=browser.find_element_by_class_name('lastPager')  
+            elem = browser.find_element_by_class_name("lastPager")
             elem.click()
             time.sleep(5)
-            counter2+=1
+            counter2 += 1
             print(counter2)
         # if no new pages finish
         else:
             print("Scraping ended succesfully!")
             break
-        
